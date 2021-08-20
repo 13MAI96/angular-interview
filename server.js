baba1000d0373b2c7274d20985d488cb199e6728
Agustin Iglesias <marceloagustiniglesias@gmail.com>
2021-08-20 19:27:54 -0300

    Correcciones para Heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/angular-interview'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/angular-interview/index.html'));
+});
+
+app.listen(process.env.PORT || 5000)
